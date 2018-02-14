d6cb4974dce87b628de83a27fcb161c628ec2184
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-02-14 09:49:59 -0200

    added link to plugin

--- template-parts/content-builder.php
+++ template-parts/content-builder.php
@@ -147,12 +147,12 @@
 
 			<div id="about" class="mt5 mb3 measure lh-copy nested-links">
 				<h2 class="ma0 f4 b">About</h2>
-				<p class="ma0">This project was created by <a href="https://luizpb.com" class="link">Luiz Bills</a> and uses <strong>WordPress Modular CSS</strong> as backend. Anyone is welcome to suggest any improvement, report issues and contribute to this project on <a href="https://github.com/luizbills/wp-modular-css" class="link">Github</a>.</p>
-				<p class="ma0"><p><a class="github-button" href="https://github.com/luizbills/wp-modular-css" data-icon="octicon-star"  data-show-count="true" aria-label="Star luizbills/wp-modular-css on GitHub">Star</a></p></p>
+				<p class="ma0">This project was created by <a href="https://luizpb.com" class="link">Luiz Bills</a> and uses <a href="https://wordpress.org/plugins/wp-modular-css/" rel="nofollow" class="link"><strong>WordPress Modular CSS</strong></a> as backend. Anyone is welcome to suggest any improvement, report issues and contribute to this project on <a href="https://github.com/luizbills/wp-modular-css" class="link">Github</a>.</p>
+				<p class="ma0"><p><a rel="nofollow" class="github-button" href="https://github.com/luizbills/wp-modular-css" data-icon="octicon-star"  data-show-count="true" aria-label="Star luizbills/wp-modular-css on GitHub">Star</a></p></p>
 			</div>
 		</main>
 
 		<footer class="mw8 center">
-			<p class="mt4 mb3 b f6 gray">Made with Tachyons and ♥ in Brazil.</p>
+			<p class="mt4 mb3 b f6 gray">Made with Tachyons, WordPress and ♥ in Brazil.</p>
 		</footer>
 	</div>
