1782e4caec508c6843742dcc7dba54c21aa5cb8f
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-02-13 23:26:03 -0200

    v1.0.1 (#1)
    * Update content-builder.php
    
    * Update style.css

--- template-parts/content-builder.php
+++ template-parts/content-builder.php
@@ -147,7 +147,7 @@
 
 			<div id="about" class="mt5 mb3 measure lh-copy nested-links">
 				<h2 class="ma0 f4 b">About</h2>
-				<p class="ma0">This project was created by <a href="" class="link">Luiz Bills</a> and uses <strong>WordPress Modular CSS</strong> as backend. Anyone is welcome to suggest any improvement, report issues and contribute to this project on <a href="https://github.com/luizbills/wp-modular-css" class="link">Github</a>.</p>
+				<p class="ma0">This project was created by <a href="https://luizpb.com" class="link">Luiz Bills</a> and uses <strong>WordPress Modular CSS</strong> as backend. Anyone is welcome to suggest any improvement, report issues and contribute to this project on <a href="https://github.com/luizbills/wp-modular-css" class="link">Github</a>.</p>
 				<p class="ma0"><p><a class="github-button" href="https://github.com/luizbills/wp-modular-css" data-icon="octicon-star"  data-show-count="true" aria-label="Star luizbills/wp-modular-css on GitHub">Star</a></p></p>
 			</div>
 		</main>
